d82825dacb94b917fcef7083ef6b2b937162bea3
Sven Baumann <baumann.sv@gmail.com>
2021-01-20 11:56:20 +0100

    Add the backend fields

---
+++ src/Resources/contao/languages/de/tl_form_field.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This file is part of contaoblackforest/contao-form-attribute-bundle.
+ *
+ * (c) 2014-2019 The Contao Blackforest team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contaoblackforest/contao-form-attribute-bundle
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2014-2019 The Contao Blackforest team.
+ * @license    https://github.com/contaoblackforest/contao-form-attribute-bundle/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+declare(strict_types=1);
+
+$GLOBALS['TL_LANG']['tl_form_field']['formFieldAttribute'] = [
+    'Formularfeld Attribut',
+    'Dies dient dazu, dem Formularfeld zusätzliche Attribute hinzuzufügen.'
+];
+$GLOBALS['TL_LANG']['tl_form_field']['labelAttribute']     = [
+    'Feldbezeichnung Attribut',
+    'Dies dient dazu, dem Label zusätzliche Attribute hinzuzufügen.'
+];
+
+$GLOBALS['TL_LANG']['tl_form_field']['options']['formFieldAttribute'] = 'Formularfeld Attribut';
+$GLOBALS['TL_LANG']['tl_form_field']['options']['labelAttribute']     = 'Feldbezeichnung Attribut';
